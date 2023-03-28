ebeb42ebec98042e9444de1d719327434d4d2499
Luis Avalos <lmavalos@gmail.com>
2023-03-27 18:59:38 -0600

    complete commit

--- terraform.tf
+++ terraform.tf
@@ -3,12 +3,13 @@
 
 terraform {
 
+/*
   cloud {
     workspaces {
       name = "learn-terraform-lambda-api-gateway"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
